9a83d17da87fe78346e29cd798380bed1d0879a5
Ally Netzel <netzelally@gmail.com>
2018-05-28 13:11:35 +0000

    automatically backed up by learn

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -6,22 +6,22 @@ describe "GET '/' - Greeting Form" do
     visit '/'
     expect(page.body).to include("Welcome!")
     describe "POST '/greet' - User Greeting" do
-  it 'greets the user personally based on their user_name in the form' do
-    visit '/'
-
-    fill_in(:user_name, :with => "Avi")
-    click_button "Submit"
-
-    expect(page).to have_text("Hi Avi, nice to meet you!")
+      it 'greets the user personally based on their user_name in the form' do
+        visit '/'
+        
+        fill_in(:user_name, :with => "Avi")
+        click_button "Submit"
+        
+        expect(page).to have_text("Hi Avi, nice to meet you!")
+      end
+    end
+    
   end
-end
-
-  end
-
+  
   # New test
   it 'has a greeting form with a user_name field' do
     visit '/'
-
+    
     expect(page).to have_selector("form")
     expect(page).to have_field(:user_name)
   end
@@ -30,10 +30,10 @@ end
 describe "POST '/greet' - User Greeting" do
   it 'greets the user personally based on their user_name in the form' do
     visit '/'
-
+    
     fill_in(:user_name, :with => "Avi")
     click_button "Submit"
-
+    
     expect(page).to have_text("Hi Avi, nice to meet you!")
   end
 end
