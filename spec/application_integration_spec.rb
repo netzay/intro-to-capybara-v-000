2a3527fd03f6bb9b6ee61be3d62f25a9d9d2467e
Ally Netzel <netzelally@gmail.com>
2018-05-28 13:11:25 +0000

    automatically backed up by learn

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -5,6 +5,17 @@ describe "GET '/' - Greeting Form" do
   it 'welcomes the user' do
     visit '/'
     expect(page.body).to include("Welcome!")
+    describe "POST '/greet' - User Greeting" do
+  it 'greets the user personally based on their user_name in the form' do
+    visit '/'
+
+    fill_in(:user_name, :with => "Avi")
+    click_button "Submit"
+
+    expect(page).to have_text("Hi Avi, nice to meet you!")
+  end
+end
+
   end
 
   # New test
